d6e428b8bb84df4b7d68449a21e6d14767654902
Satheeshwaran J <satheesh4590@gmail.com>
2020-05-20 18:40:55 +0400

    Initial Commit

---
+++ AwesomeWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeWeather
+//
+//  Created by Satheeshwaran Jeevanandan - (Digital) on 20/05/20.
+//  Copyright © 2020 Satheeshwaran Jeevanandan - (Digital). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
